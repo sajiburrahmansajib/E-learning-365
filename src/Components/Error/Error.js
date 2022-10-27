65fe2e7f9ec6d358b1a32a7615960a28a0a8ac5d
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-10-27 16:52:05 +0600

    Readme file updated

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = () => {
     return (
         <div>
-            <h1 className='mt-60 text-red-500 text-xl'>Unknown Path , Path Not Found 404 </h1>
+            <h1 className='mt-60 text-red-500 text-xl text-center mb-28'>Unknown Path , Path Not Found 404 !!! </h1>
         </div>
     );
 };
